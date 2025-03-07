71d99cdd326e2f2bc0720baf70369726bfa9d7ec
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-07 17:09:28 +0300

    Протестирован метод get_book_genre: - Тест test_get_book_genre_valid_genre проверяет правильность возвращаемого жанра для существующей книги 'Аватар'. - Тест test_get_book_genre_non_existing_book проверяет, что возвращается None для несуществующей книги 'Неизвестная книга'. -

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pytest
 from main import BooksCollector
 
+
 class TestBooksCollector:
 
     # Тестирование метода __init__ класса BooksCollector
@@ -22,7 +23,7 @@ class TestBooksCollector:
         assert collector.genre_age_rating == ['Ужасы', 'Детективы']
 
     # Тестирование метода add_new_book
-        #Добавление книги без указания жанра с валидной длинной
+    # Добавление книги без указания жанра с валидной длинной
     @pytest.mark.parametrize('book_name', ['A', 'B' * 10, 'c' * 40])
     def test_add_new_book_valid_name_true(self, book_name):
         collector = BooksCollector()
@@ -30,6 +31,7 @@ class TestBooksCollector:
         assert collector.books_genre == {book_name: ''}
 
         # Добавление книги без указания жанра с невалидной длинной
+
     @pytest.mark.parametrize('book_name', ['', 'B' * 41, 'c' * 50])
     def test_add_new_book_invalid_len_name_true(self, book_name):
         collector = BooksCollector()
@@ -48,12 +50,25 @@ class TestBooksCollector:
         assert collector.books_genre == {name: genre}
 
         # Установка невалидного жанра
+
     @pytest.mark.parametrize('name, invalid_genre', [('Репка', 'Садоводство')])
-    def test_set_book_genre_invalid_genre(self, name,  invalid_genre):
+    def test_set_book_genre_invalid_genre(self, name, invalid_genre):
         collector = BooksCollector()
         collector.add_new_book(name)
         original_genre = collector.books_genre.get(name)
         collector.set_book_genre(name, invalid_genre)
         assert collector.books_genre[name] == original_genre
 
+        # Тестирование метода get_book_genre
+            # Использование валидного жанра
 
+    def test_get_book_genre_valid_genre(self, setup_books_collector):
+        collector = setup_books_collector
+        genre = collector.get_book_genre('Аватар')
+        assert genre == 'Фантастика'
+
+        # Узнать жанр несуществующей книги
+    def test_get_book_genre_non_existing_book(self, setup_books_collector):
+        collector = setup_books_collector
+        genre = collector.get_book_genre('Неизвестная книга')
+        assert genre is None
