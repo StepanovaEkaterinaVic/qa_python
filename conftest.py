3b9f0d420592481b689db669392e44eb7fb33a2d
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-08 22:05:31 +0300

    В фикстуру setup_books_collector добавлена книга ('Три кота', 'Мультфильмы')

--- conftest.py
+++ conftest.py
@@ -9,8 +9,10 @@ def setup_books_collector():
     collector.add_new_book('Аватар')
     collector.add_new_book('Лучший сыщик')
     collector.add_new_book('Солярис')
+    collector.add_new_book('Три кота')
     collector.set_book_genre('Аватар', 'Фантастика')
     collector.set_book_genre('Солярис', 'Фантастика')
     collector.set_book_genre('Лучший сыщик', 'Детективы')
+    collector.set_book_genre('Три кота', 'Мультфильмы')
     return collector
 
